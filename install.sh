efbc0dc5dff2f72eb13800ebebdd28be40f4b1f5
Matthias Hollerbach <matzehollerbach@web.de>
2018-01-04 18:51:56 +0100

    added sudo keepalive

--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@ set -o nounset
 REPO="https://github.com/KingLoui/ansible-macos-playbook.git"
 TARGET_DIR="$HOME/devenv/ansible-macos-playbook"
 
+# Ask for sudo password upfront if needed
+if ! sudo -n true 2>/dev/null; then
+    echo -en "Enter Sudo "
+    sudo -v
+    # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+fi
+
 if test ! $(which brew); then
   echo "Installing Homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
